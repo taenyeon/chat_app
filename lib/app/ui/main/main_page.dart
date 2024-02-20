554565905c921a7a269086609c24accd2aa38a24
taeyeonKim <tae8753@gmail.com>
2024-02-21 00:03:24 +0900

    chat_app init

--- lib/app/ui/main/main_page.dart
+++ lib/app/ui/main/main_page.dart
@@ -8,25 +8,6 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FlexibleGridView(
-      axisCount: GridLayoutEnum.twoElementsInRow,
-      crossAxisSpacing: 8,
-      mainAxisSpacing: 8,
-      children: List.generate(
-        4,
-        (index) => Container(
-          color: colors[Random().nextInt(colors.length)],
-          height: 600 + Random().nextInt(100).toDouble(),
-        ),
-      ),
-    );
+    return Container();
   }
 }
-
-List<Color> colors = [
-  Colors.white30,
-  Colors.limeAccent,
-  Colors.white,
-  Colors.blue,
-  Colors.orangeAccent
-];
